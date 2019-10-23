896db5031cf8e5e75c3996f4f4817999a285abf8
Devair Luiz Moitim <dmoitim@gmail.com>
2019-10-23 05:28:43 -0300

    Correções ESLint

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,85 @@
 <template>
-  <div>
-    <div id="slogan" class="text-center">
-      <h1>Gerador de Nome</h1>
-      <br/>
-      <h6 class="text-secondary">Gerador de nomes utilizando VueJS, GraphQL e NodeJS</h6>
-    </div>
-    <div id="main">
-      <div class="container">
-        <div class="row">
-          <div class="col-md">
-            <h5>Prefixos <span class="badge badge-info">{{prefixes.length}}</span></h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
-                    {{ prefix }}
-                  </li>
-                </ul>
-                <br/>
-                <input class="form-control" type="text" placeholder="Digite o prefixo" />
-              </div>
-            </div>
-          </div>
-          <div class="col-md">
-            <h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
-            <div class="card">
-              <div class="card-body">
-                <ul class="list-group">
-                  <li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
-                    {{ sufix }}
-                  </li>
-                </ul>
-                <br/>
-                <input class="form-control" type="text" placeholder="Digite o sufixo" />
-              </div>
-            </div>
-          </div>
-        </div>
-        <br/>
-        <h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
-        <div class="card">
-          <div class="card-body">
-            <ul class="list-group">
-              <li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
-                {{ domain }}
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
-    </div>
-  </div>
+	<div>
+		<div id="slogan" class="text-center">
+			<h1>Gerador de Nome</h1>
+			<br/>
+			<h6 class="text-secondary">Gerador de nomes utilizando VueJS, GraphQL e NodeJS</h6>
+		</div>
+		<div id="main">
+			<div class="container">
+				<div class="row">
+					<div class="col-md">
+						<h5>Prefixos <span class="badge badge-info">{{prefixes.length}}</span></h5>
+						<div class="card">
+							<div class="card-body">
+								<ul class="list-group">
+									<li class="list-group-item" v-for="prefix in prefixes" v-bind:key="prefix">
+										{{ prefix }}
+									</li>
+								</ul>
+								<br/>
+								<input class="form-control" type="text" placeholder="Digite o prefixo" />
+							</div>
+						</div>
+					</div>
+					<div class="col-md">
+						<h5>Sufixos <span class="badge badge-info">{{ sufixes.length }}</span></h5>
+						<div class="card">
+							<div class="card-body">
+								<ul class="list-group">
+									<li class="list-group-item" v-for="sufix in sufixes" v-bind:key="sufix">
+										{{ sufix }}
+									</li>
+								</ul>
+								<br/>
+								<input class="form-control" type="text" placeholder="Digite o sufixo" />
+							</div>
+						</div>
+					</div>
+				</div>
+				<br/>
+				<h5>Domínios <span class="badge badge-info">{{ domains.length }}</span></h5>
+				<div class="card">
+					<div class="card-body">
+						<ul class="list-group">
+							<li class="list-group-item" v-for="domain in domains" v-bind:key="domain">
+								{{ domain }}
+							</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css"
-import "font-awesome/css/font-awesome.css"
+import "bootstrap/dist/css/bootstrap.css";
+import "font-awesome/css/font-awesome.css";
 
 export default {
-  name: "app",
-  data: function() {
-    return {
-      prefixes: ['Air', 'Jet', 'Flight'],
-      sufixes: ['Hub', 'Station', 'Mart'],
-      domains: ['AirHub', 'JetHub', 'FlightHub',
-                'AirStation', 'JetStation', 'FlightStation',
-                'AirMart', 'JetMart', 'FlightMart']
-    };
-  }
+	name: "app",
+	data: function() {
+		return {
+			prefixes: ["Air", "Jet", "Flight"],
+			sufixes: ["Hub", "Station", "Mart"],
+			domains: ["AirHub", "JetHub", "FlightHub",
+				"AirStation", "JetStation", "FlightStation",
+				"AirMart", "JetMart", "FlightMart"]
+		};
+	}
 };
 </script>
 
 <style>
 #slogan {
-  margin-top: 30px;
-  margin-bottom: 30px;
+	margin-top: 30px;
+	margin-bottom: 30px;
 }
 
 #main {
-  background-color: #f1f1f1;
-  padding-top: 30px;
-  padding-bottom: 30px;
+	background-color: #f1f1f1;
+	padding-top: 30px;
+	padding-bottom: 30px;
 }
 </style>
