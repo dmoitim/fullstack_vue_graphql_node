98935d3f5d769f6672f274ec7a55608e78f8e93f
Devair Luiz Moitim <dmoitim@gmail.com>
2019-10-26 07:12:07 -0300

    Aula 05

--- client/src/components/DomainList.vue
+++ client/src/components/DomainList.vue
@@ -44,8 +44,7 @@
 </template>
 
 <script>
-import "bootstrap/dist/css/bootstrap.css";
-import "font-awesome/css/font-awesome.css";
+import axios from "axios/dist/axios";
 import AppItemList from "./AppItemList";
 
 export default {
@@ -55,8 +54,8 @@ export default {
 	},
 	data: function() {
 		return {
-			prefixes: ["Air", "Jet", "Flight"],
-			sufixes: ["Hub", "Station", "Mart"]
+			prefixes: [],
+			sufixes: []
 		};
 	},
 	methods: {
@@ -89,6 +88,30 @@ export default {
 			}
 			return domains;
 		}
+	},
+	created() {
+		axios({
+			url: "http://localhost:4000",
+			method: "post",
+			data: {
+				query: `
+					{
+						prefixes: items (type: "prefix"){
+							id
+							type
+							description
+						}
+						sufixes: items (type: "sufix"){
+							description
+						}
+					}
+				`
+			}
+		}).then(response => {
+			const query = response.data;
+			this.prefixes = query.data.prefixes.map(prefix => prefix.description);
+			this.sufixes = query.data.sufixes.map(sufix => sufix.description);
+		});
 	}
 };
 </script>
